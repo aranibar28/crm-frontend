580c16ef98a060c9db5bac86cd3e814b52ac1bde
Gerson Aranibar <aranibargerson28@gmail.com>
2022-11-21 23:11:49 -0500

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   email: 'admin@gmail.com',
-  base_url: 'https://crm-backend-b9qr.onrender.com/api',
+  base_url: 'https://crm-backend-b5m1.onrender.com/api',
   sunat: 'https://dniruc.apisperu.com/api/v1',
   token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFyYW5pYmFyZ2Vyc29uMjhAZ21haWwuY29tIn0.ObOQyED4Nhtvm-b7bQpbxYGygJxqQqvop4VuqonMi-8',
 };
